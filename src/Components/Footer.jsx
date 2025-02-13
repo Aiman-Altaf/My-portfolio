c8c9b05ba2d4f2e72d46804c4964032f3b89f745
Aiman-Altaf <iymanaltaf09876@gmail.com>
2025-02-13 18:34:20 +0530

    change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -61,7 +61,7 @@ const Footer = (props) => {
         }}
       >
         {email && (
-          <a href={`mailto:${email}`}>
+          <a href={`mailto:iymanaltaf09876@gmail.com${email}`}>
             <img src={envelopeIcon} alt="email" className="socialIcon" />
           </a>
         )}
@@ -71,7 +71,7 @@ const Footer = (props) => {
           </a>
         )}
         {gitHub && (
-          <a href={`https://github.com/${gitHub}`} target="_blank" rel="noopener noreferrer">
+          <a href={`https://github.com/Aiman-Altaf/${gitHub}`} target="_blank" rel="noopener noreferrer">
             <img src={gitHubIcon} alt="GitHub" className="socialIcon" />
           </a>
         )}
